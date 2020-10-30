18500cd600c5089b745fb7f6fcd055ee694657ea
sultan-alhowaymel <aleph@Macintosh.local>
2020-10-30 19:23:06 +0300

    v2

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const saction1 = document.getElementById("header");
+
+saction1.style.color = "red";
+
+const saction2 = document.getElementById("saction1");
+
+saction2.style.color = "red";
+
+console.log(Math.PI);
