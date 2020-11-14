91b7ae48444841ddd693b9ec7194041589116ac8
sultan-alhowaymel <aleph@Macintosh.local>
2020-11-14 17:22:03 +0300

    v10

--- javascript.js
+++ javascript.js
@@ -1,9 +1,2 @@
 const saction1 = document.getElementById("header");
-
-saction1.style.color = "red";
-
-const saction2 = document.getElementById("saction1");
-
-saction2.style.color = "red";
-
-console.log(Math.PI);
+ 
