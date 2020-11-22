d4afd75dab2b3fe278f74f117e05bdea9133433a
sultan-alhowaymel <aleph@Macintosh.local>
2020-11-22 19:35:14 +0300

    22-11-2020

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
  
+// this is function for scrollInto View Method 
+
+
 function myFunction1() {
   var elmnt = document.getElementById("section1");
   elmnt.scrollIntoView();
@@ -17,3 +20,19 @@ function myFunction4() {
   var elmnt = document.getElementById("section4");
   elmnt.scrollIntoView();
 }
+
+// this is function for getBoundingClientRect() Method 
+
+function myFunction5() {
+  var s = document.getElementById("section");
+  var rect = s.getBoundingClientRect();
+  x = rect.top;
+  
+    if (x = 41.84375) {
+        alert ("Left: " + x + " , Height: " + h);
+
+    }
+    else
+         alert ("Left: " + x + " , Height: " + h);
+};
+myFunction5();
